e949fa3de0c0fd97aa3052418ae45835fb18e7d8
rakouzaine <rnsouni@gmail.com>
2025-10-09 09:18:30 +0200

    Casse Brique 2

--- Casse_Brique_Appel_Fonction.py
+++ Casse_Brique_Appel_Fonction.py
@@ -10,8 +10,80 @@ quoi : programmation du jeu casse brique sur TKinter
 
 from tkinter import * 
 
-def on_begin():
-    print("Le jeu commence…")
+
+        
+
+
+class Page_Presentation : 
+    
+class Page_Jeu : 
+    
+    
+class Creation_Brique :
+    # creer la forme et la couleur de la brique
+    # les aligner une par une en haut de la page jeu
+    
+    
+class rebond_cadre : 
+    # faire rebondir la balle sur le cadre du jeu     
+    
+class Destruction_Brique :   
+    # supprimer la brique
+    # Supperimer brique = True
+    
+    
+    
+class Score : 
+    def __init__(self , brique):
+        self.__brique = brique
+        self.__point = []
+    def compte_point(self):
+        if brique ==     :
+            
+            
+    
+class Creation_Balle : 
+class Mouvement_Balle :
+    # mouvement physique dans l'espace 
+    # rebond de la balle sur la brique , prendre True 
+   
+class Mouvement_plateforme :  
+class Deplacement_Clavier_plateforme : 
+    
+class vie :
+    # compter le nb de vie 
+    # reinitialiser le jeu ( remtettre la page _Jeu)
+    
+class Fin_Jeu : 
+    
+    
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 root = tk.Tk()
 root.title("Jeu du casse-briques")
@@ -23,6 +95,9 @@ begin_button = tk.Button(root, text="Begin", command=on_begin)
 begin_button.pack(side=tk.LEFT, padx=5, pady=5)
 
 canvas = Canvas(root , width = 800 , height = 400, bg = "black")
+
+"""affichage score """
+text
 
 quit_button = tk.Button(root, text="Quit", command=root.destroy)
 quit_button.pack(side=tk.LEFT, padx=5, pady=5)
